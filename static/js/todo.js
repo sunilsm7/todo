fbdc703d92fab7707f48b2338934b6b37bd3b81e
Sunil <sunil9766@gmail.com>
2017-09-10 13:27:56 +0530

    detail and delete serializer

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,7 +1,17 @@
 var app = angular.module("toDo", []);
-app.controller("toDoController", function($scope){
-	$scope.todoList = [{todoText:'Finish this app', done:false}];
+app.controller("toDoController", function ($scope, $http){
+	// $scope.todoList = [{todoText:'Finish this app', done:false}];
 	
+	$http.get('/todo/api').then(function (response){
+		$scope.todoList = [];
+		console.log(response.data);
+		for (var i=0; i<response.data.length; i++){
+			var todo = {};
+			todo.todoText = response.data[i].text
+			todo.done = response.data[i].done
+			$scope.todoList.push(todo)
+		}
+	});
 	$scope.todoAdd = function(){
 		$scope.todoList.push({todoText:$scope.todoInput, done:false});
 		$scope.todoInput = '';
--- todo/urls.py
+++ todo/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from todo.views import ToDoView
+from todo.views import ToDoListView, ToDoDetailView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name = 'todo/index.html')),
-    url(r'^todo/api/',ToDoView.as_view()),
+    url(r'^todo/api/$',ToDoListView.as_view()),
+    url(r'^todo/api/(?P<pk>[0-9]+)/$',ToDoDetailView.as_view()),
     url(r'^todo/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
