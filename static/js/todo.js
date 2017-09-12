c2348bcd2145da8f8a932a17f8ab2da85edb0a26
Sunil <sunil9766@gmail.com>
2017-09-12 11:59:48 +0530

    put and delete request

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,17 +1,28 @@
 var app = angular.module("toDo", []);
+
 app.controller("toDoController", function ($scope, $http){
 	// $scope.todoList = [{todoText:'Finish this app', done:false}];
-	
+	$http.defaults.xsrfCookieName = 'csrftoken';
+	$http.defaults.xsrfHeaderName = 'X-CSRFToken';
+
 	$http.get('/todo/api').then(function (response){
 		$scope.todoList = [];
-		console.log(response.data);
+		
 		for (var i=0; i<response.data.length; i++){
 			var todo = {};
 			todo.todoText = response.data[i].text
 			todo.done = response.data[i].done
+			todo.id = response.data[i].id
 			$scope.todoList.push(todo)
 		}
 	});
+
+	$scope.saveData = function(){
+		var data = {text: $scope.todoInput, done: false};
+		$http.put('/todo/api/', data);
+		console.log(data);
+	};
+
 	$scope.todoAdd = function(){
 		$scope.todoList.push({todoText:$scope.todoInput, done:false});
 		$scope.todoInput = '';
@@ -20,10 +31,17 @@ app.controller("toDoController", function ($scope, $http){
 	$scope.remove = function(){
 		var oldList = $scope.todoList;
 		$scope.todoList = [];
-		angular.forEach(oldList, function(x){
-			if (!x.done) $scope.todoList.push(x);
-		});
+		angular.forEach(oldList, function(todo){
+			if (todo.done) {
+				$http.delete('/todo/api/' + todo.id +'/');
+				
+			}else{
+				$scope.todoList.push(todo);
+			}
 
+		});
+		
+		
 	};
 
 });
